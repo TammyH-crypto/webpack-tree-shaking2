bb0bf3add5650d31eb875e6a700046d76c39dcc7
Tammy Higgins <tmhiggins1986@gmail.com>
2025-04-23 20:15:59 -0500

    New project

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+"use strict";
+
+import path from "path";
+import { FileURLToPath } from "url";
+
+const __filename = FileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  mode: "development",
+ 
+};
